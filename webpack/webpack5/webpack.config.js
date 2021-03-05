8acbfb0246f271471d04fa40a8c111e10a374a6b
juan <liu_juan_juan@yeah.net>
2021-03-05 10:46:21 +0800

    webpack基础

---
+++ webpack/webpack5/webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    index: './src/index.js',
+    search: './src/search.js'
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].js'
+  }
+}
